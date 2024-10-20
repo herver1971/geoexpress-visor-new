6bf7ec825527fc1ce1d03b016ea68b9752d40655
Hernan <hernan.lopez@gmail.com>
2024-10-20 12:24:27 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,9 +57,9 @@ latex_additional_files = ['_static/images/cabecerakan.png',
 '_static/images/encabezado.png']
 
 latex_elements = {
-    'pointsize': '11pt',
-    'classoptions': ',openany',
-    'passoptionstopackages': r'''
+  'pointsize': '11pt',
+  'classoptions': ',openany',
+  'passoptionstopackages': r'''
 \PassOptionsToPackage{svgnames}{xcolor}
 ''',
     'fontpkg': r'''
@@ -76,11 +76,16 @@ latex_elements = {
 \usepackage[titles]{tocloft}
 \usepackage{xcolor}
 \usepackage{geometry}
-
-
+\usepackage{setspace}
 \usepackage{fancyhdr}
+\usepackage{titlesec}
 \pagestyle{fancy}
 
+\geometry{
+  topmargin=2.5cm,  % Ajusta este valor para controlar el espacio superior
+  headheight=5pt,  % Altura del encabezado
+  headsep=50pt,  % Espacio entre la línea del encabezado y el contenido del texto
+}
 
 % Definir la ruta de búsqueda para imágenes
 \graphicspath{{_static/images/}}
@@ -104,19 +109,41 @@ latex_elements = {
 % Definir la macro de versión a partir del valor en conf.py
 \newcommand{\therelease}{''' + release + r'''}
 
-    % Redefinir \sphinxmaketitle para ajustar la posición y formato del título
-    \renewcommand{\sphinxmaketitle}{%
-      \begingroup
-        \begin{center}
-          \vspace*{6cm}  % Mover el título hacia abajo
-          {\Huge \textbf{\color[RGB]{245,63,97} \thetitle}}  % Título del proyecto en grande y color magenta
-          \vspace{0.5cm}  % Espacio entre el título y la versión
-          \\
-          {\Large \textbf{\color[RGB]{245,63,97} Versión:} \color[RGB]{245,63,97} \therelease}  % Mostrar la versión en grande y en color magenta
-        \end{center}
-      \endgroup
-    }
+% Redefinir \sphinxmaketitle para ajustar la posición y formato del título
+\renewcommand{\sphinxmaketitle}{%
+  \begingroup
+    \pagestyle{empty}
+    \begin{flushleft}  % Alinear a la izquierda
+      \vspace*{6cm}  % Mover el título hacia abajo
+      {\fontsize{36}{40}\selectfont \textbf{\color[RGB]{245,63,97} \thetitle}}  % Título del proyecto en grande y color magenta
+      \vspace{0.5cm}  % Espacio entre el título y la versión
+      \\
+      {\Large \textbf{\color[RGB]{245,63,97} Versión:} \color[RGB]{245,63,97} \therelease}  % Mostrar la versión en grande y en color magenta
+    \end{flushleft}
+    \clearpage
+  \endgroup
+  \pagestyle{plain}  % Volver al estilo 'plain' después de la portada
+}
 
+% Configuración del estilo de capítulo
+\titleformat{\chapter}[display]  % El formato "display" asegura que el título del capítulo y su nombre se muestren en líneas separadas
+{\normalfont\Huge\bfseries\color[RGB]{245,63,97}}  % Estilo y color
+{Capítulo \thechapter}  % Título del capítulo
+{20pt}  % Espacio entre "Capítulo X" y el nombre del capítulo
+{\Huge\bfseries}  % Formato del nombre del capítulo
+
+% Ajuste del espaciado entre el número de capítulo y el nombre
+\titlespacing*{\chapter}{0pt}{-20pt}{20pt}  % Ajusta estos valores para acercar al encabezado
+
+% Establecer el interlineado para el cuerpo del texto
+\AtBeginDocument{
+  \setstretch{1.5}  % Ajuste de interlineado: 1.5 es un buen valor para aumentar la separación entre líneas
+}
+
+% Configurar color del texto en gris oscuro
+\AtBeginDocument{
+  \color[RGB]{80,80,80}  % Establece un color gris oscuro para el texto principal
+}
 
 % Configuración de la tabla de contenidos y estilos
 \cftsetpnumwidth{1.25cm}
@@ -126,25 +153,43 @@ latex_elements = {
 \setlength{\cftsecnumwidth}{1.25cm}
 \renewcommand{\cftsecpagefont}{\color{red}}
 
-    % Configurar estilo de las páginas de los capítulos
-    \fancypagestyle{normal}{ % Estilo para las páginas de los capítulos
-        \fancyhf{}  % Eliminar cualquier encabezado y pie por defecto
-        \fancyhead[C]{\includegraphics{encabezado.png}}        
-        \renewcommand{\headrulewidth}{0pt}  % Eliminar la línea del encabezado
-        \renewcommand{\footrulewidth}{0pt}  % Eliminar la línea del pie de página
-        \rfoot[\thepage]{\thepage}  % Numerar las páginas de los capítulos en el centro
+
+% Configurar estilo de las páginas de los capítulos
+   \fancypagestyle{normal}{ % Estilo para las páginas de los capítulos
+    \fancyhf{}  % Eliminar cualquier encabezado y pie por defecto
+    \fancyhead[R]{\includegraphics{encabezado.png}}        
+    \renewcommand{\headrulewidth}{1pt}  % Eliminar la línea del encabezado
+    \renewcommand{\footrulewidth}{0pt}  % Eliminar la línea del pie de página
+    \rfoot[\thepage]{\thepage}  % Numerar las páginas de los capítulos en el centro
+
+    % Cambiar el color de la línea del encabezado
+    \renewcommand{\headrule}{
+        \begingroup\color[RGB]{245,63,97}\hrule width\headwidth height\headrulewidth\endgroup
+  }
+}
+
+% Estilo para la primera página de los capítulos
+\fancypagestyle{plain}{
+    \fancyhf{}  
+    \fancyhead[R]{\includegraphics{encabezado.png}}  
+    \renewcommand{\headrulewidth}{1pt}  
+    \rfoot[\thepage]{\thepage}  
+    
+    % Cambiar el color de la línea del encabezado
+    \renewcommand{\headrule}{
+        \begingroup\color[RGB]{245,63,97}\hrule width\headwidth height\headrulewidth\endgroup
     }
+}
 
     % Asegurar que solo las páginas de los capítulos muestren la numeración en arábigos
     \renewcommand{\chapterpagestyle}{normal}  % Aplicar estilo 'normal' a las páginas de los capítulos
     \pagenumbering{arabic}  % Usar numeración arábiga para las páginas del capítulo
 
-% Establecer color y alineación predeterminados para el contenido principal
-    \AtBeginDocument{%
-      \color{black}  % Establece el color del texto principal a negro
-      \raggedright   % Alinea el texto a la izquierda sin justificar
-    }
-
+% Combinar configuraciones para el inicio del documento y contenido principal
+\AtBeginDocument{%
+  \color{black}  % Establece el color del texto principal a negro
+  \raggedright   % Alinea el texto a la izquierda sin justificar
+}
 ''',
     'sphinxsetup': '''
     InnerLinkColor={RGB}{245,63,97},
